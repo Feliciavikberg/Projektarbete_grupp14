b702295a3c946f33ae7bad229047b9c7343f45e0
Erik Lindström <erik.lindstrom@live.se>
2016-03-08 15:54:02 +0100

    Hej

---
+++ IIProjectClient/Controllers/PassageinfoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IIProjectClient.Controllers
+{
+    public class PassageinfoController : Controller
+    {
+        // GET: Passageinfo
+        public ActionResult Index()
+        {
+            PassageServiceReference.PassageInfoServiceClient passageClient = new PassageServiceReference.PassageInfoServiceClient();
+            passageClient.Test();
+            return View();
+        }
+        
+    }
+}
